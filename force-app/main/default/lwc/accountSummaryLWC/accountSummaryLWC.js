1dfb747902595eed1cab73df35336b9e7df75f26
Soumen Haldar <soumenhaldar841@yahoo.com>
2021-09-08 15:17:55 +0530

    Another Commit

--- force-app/main/default/lwc/accountSummaryLWC/accountSummaryLWC.js
+++ force-app/main/default/lwc/accountSummaryLWC/accountSummaryLWC.js
@@ -13,58 +13,49 @@ export default class accountSummary extends LightningElement {
     @track opp;
     @track cs;
     connectedCallback(){
-                        fetchAccount()
-                        .then(result => {
-                                        this.acc = result;
+        fetchAccount()
+        .then(result => {
+            this.acc = result;
+            console.log(JSON.stringify(result));
+            console.log("result",this.acc);
+        })
 
-                                        console.log(JSON.stringify(result));
-                                        console.log("result",this.acc);
-                                        })
-
-                        }
-
+    }
 
     contactFetch(event){
-                            this.message = event.target.value;
-                            console.log('Contact Id-->'+this.message);
-                            fetchContact({accountId : this.message})
+        this.message = event.target.value;
+        console.log('Contact Id-->'+this.message);
+        fetchContact({accountId : this.message})
+        .then(result => {
+              this.con = result;
+              console.log(JSON.stringify(result));
+              console.log("result1",this.con);
+        })
+        .catch(error =>{
+            this.error = error;
+        })
 
-                            .then(result => {
-                                                this.con = result;
-
-                                                console.log(JSON.stringify(result));
-                                                console.log("result1",this.con);
-                                            })
-                            .catch(error =>{
-                            this.error = error;
-
-                                            })
-
-                            this.msg = event.target.value;
-                            console.log('Oppoertunity Id-->'+this.msg);
-                            fetchOpportunity({accountId : this.msg})
-                            .then(result => {
-                                              this.opp = result;
-
-                                              console.log(JSON.stringify(result));
-                                              console.log("result2",this.opp);
-                                            })
-                            .catch(error =>{
-                                            this.error = error;
-
-                                            })
-                            this.msg = event.target.value;
-                            console.log('Case Id-->'+this.msg);
-                            fetchCase({accountId : this.msg})
-                            .then(result => {
-                                              this.cs = result;
-
-                                              console.log(JSON.stringify(result));
-                                              console.log("result2",this.cs);
-                                            })
-                            .catch(error =>{
-                                            this.error = error;
-
-                                            })
-                        }
+        this.msg = event.target.value;
+        console.log('Oppoertunity Id-->'+this.msg);
+        fetchOpportunity({accountId : this.msg})
+        .then(result => {
+            this.opp = result;
+            console.log(JSON.stringify(result));
+            console.log("result2",this.opp);
+        })
+        .catch(error =>{
+            this.error = error;
+        })
+        this.msg = event.target.value;
+        console.log('Case Id-->'+this.msg);
+        fetchCase({accountId : this.msg})
+        .then(result => {
+            this.cs = result;
+            console.log(JSON.stringify(result));
+            console.log("result2",this.cs);
+        })
+        .catch(error =>{
+            this.error = error;
+        })
+    }
 }
